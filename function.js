d843dc195052699bdefaf9ce30505aaafb73a37e
Francisco <Gimli22@gmail.com>
2022-06-18 23:49:47 +0200

    añadidos objetos y cambios en la barra lateral en Js

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-const monkeymap = initMap();
+const monkeyMap = initMap();
 const verb = defineVerbs();
 const button = document.getElementById("do");
 const screen = document.getElementById("description")
 button.onclick = verbValidation
 
 let player = {position:[0,3]}
-let currentPosition = monkeymap[player.position[0]][player.position[1]];
+let currentPosition = monkeyMap[player.position[0]][player.position[1]];
 
 
 function initMap(){
@@ -16,7 +16,8 @@ function initMap(){
         [0 ,1 ,2 ,3],
     ];
    monkey[0][0] = {description:"Ves un lago , necesitarias un barca para cruzarlo", 
-                   exits :["s","e"]},
+                   exits :["s","e"] ,
+                   items :["barca"] },
                  
    
    monkey[0][1] = {description:" Al cruzar el lago ves un inmenso campo enfrente de tí con lo que parece un bosque al fondo " ,
@@ -24,26 +25,29 @@ function initMap(){
                   
    
    monkey[0][2] = {description:"Entras en un bosque de Eucalipto a tu alrededor sientes un extraño silecio" , 
-                   exits :["w"]} , 
+                   exits :["w"] ,
+                   items :["Salvoconducto"]} , 
                  
    monkey[0][3] = {description:"Estas en una carcel antigua , el aspecto de la propia carcel te parece deteriorado , a tu lado estan los restos de varios hombres uno de los cuales tiene una carta en sus manos.",
-                   exits: ["s"] },
+                   exits: ["s"] ,
+                   items: ["Ladrillo" , "Llave"]},
                   
 
    monkey[1][0] = {description:"Delante de tí tienes un pequeño embarcadero y una tienda a su lado ",
                    exits:["n","e"]} ;
 
 
-   monkey[1][1] = {description:" Estas en la tienda de Stan , llegar hasta aqui no ha debido de ser nada facíl", 
-                   exits:["w","s"]};
-
+   monkey[1][1] = {description:" Estas en la tienda de Stan , podrias comprarle algún objeto si tuvieses dinero", 
+                   exits:["w","s"],
+                   items:["Pala"]};
 
    monkey[1][2] = {description:" Has llegado al cuartel , a tu izq ves la puerta de salida del mismo ", 
                    exits:["s","e"]};
 
 
    monkey[1][3] = {description:"Ves un pasillo largo donde esta la entrada de la carcel , ves dos puertas una de ellas a la entrada de la carcel y la otra a un pasillo oscuro.", 
-                   exits:["n","w","s"]} ;
+                   exits:["n","w","s"] , 
+                   items:["vela"] };
 
 
    monkey[2][0] = {description:"Has llegado al acceso de una playa paradisiaca a lo lejos puedes ver un barco , te sera dificil pasar un guardia custodia el puerto.", 
@@ -59,24 +63,27 @@ function initMap(){
                    
                    
    monkey[2][3] = {description:"Te adentras en un pasillo oscuro , necesitarias algo de luz para ver lo que hay en este pasillo.",
-                   exits:["n","s"]} ;
+                   exits:["n","s"] ,
+                   items:["candelabro"]};
                    
                    
-   monkey[3][0] = {description:"Has conseguido escapar de la Isla , por fín puedes convertirte en un pirata", 
+   monkey[3][0] = {description:"Si estas aqui , !Eureka! ya tienes todo lo necesario para salir de la isla", 
                    exits:["n","w"]};
                    
                    
    monkey[3][1] = {description:"Estas en la biblioteca tienes delante de tí un sillon con una mesita y varias estanterias estan a tu alrededor.",
-                   exits:["n","e"]};
+                   exits:["n","e"] ,
+                   items:[" Libro ","Carta"] };
                    
                    
-   monkey[3][2] = {description:" Si estas aqui , !Eureka! ya tienes todo lo necesario para salir de la isla ,",
-                   exits:["w"]};
+   monkey[3][2] = {description:"Te hayas ante una sala con un escritorio y un mapa desplegado , si usas Mirar en el mapa veras una X en la isla   ,",
+                   exits:["w"] ,
+                   items:["Mapa con X"] };
 
 
    monkey[3][3] = {description:"Llegas a un comedor con una larga mesa en el centro , a tu alrededor ves esculturas y cuadros , la comida que esta encima de la mesa lleva mucho tiempo podrida.",
-                   exits:["n"]};
-                   
+                   exits:["n"] ,
+                   items :["Armadura"]};
                    
   
   return monkey ;
@@ -98,9 +105,9 @@ function defineVerbs(){
             if (!validDirections.includes(direction) ){
                 return appendToOutput("Esa dirección no es idonea")
             }else{ 
-                moveValidation(direction)
-                  
-            } 
+                 moveValidation(direction)
+                 
+             } 
     }  
 
 }
@@ -125,31 +132,31 @@ function moveValidation(command){
     if(currentPosition.exits.includes(command)){
         if(command == "n"){
             player.position[0]--
-            currentPosition = monkeymap[player.position[0]][player.position[1]]
+            currentPosition = monkeyMap[player.position[0]][player.position[1]]
             showDescription();  
             return 
         }
         if(command == "s"){
             player.position[0]++
-            currentPosition = monkeymap[player.position[0]][player.position[1]]
+            currentPosition = monkeyMap[player.position[0]][player.position[1]]
             showDescription();  
             return
         }
         if(command == "w"){
             player.position[1]--
-            currentPosition = monkeymap[player.position[0]][player.position[1]]
+            currentPosition = monkeyMap[player.position[0]][player.position[1]]
             showDescription();  
             return
         }
         if(command == "e"){
             player.position[1]++
-            currentPosition = monkeymap[player.position[0]][player.position[1]]
+            currentPosition = monkeyMap[player.position[0]][player.position[1]]
             showDescription();  
             return
         } 
-        //showDescription();     
+             
     }
-     
+    //showDescription();   
 }
 function showDescription(){
     appendToOutput(currentPosition.description);
@@ -160,5 +167,11 @@ function appendToOutput(text){
     const newText = document.createTextNode(text);
     newElement.appendChild(newText);
     screen.appendChild(newElement);
+    const monkeyScroll = document.getElementById("description");
+    monkeyScroll.scrollTop = monkeyScroll.scrollHeight;
 
 }
+/* Acabar de poner los items y hacer la funcion de coger . 
+   hacer la funcion de mirar y de usar.
+   seguir completando el juego con las funciones que faltan.
+   Plantear el juego de warhammer*/
